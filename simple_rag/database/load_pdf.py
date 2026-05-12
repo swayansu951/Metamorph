80fa4091f3068693dd60d0d26775f35166d56455
swayansu951 <swayansusahu951@gmail.com>
2026-05-12 21:43:25 +0530

    resolve file upload error

--- simple_rag/database/load_pdf.py
+++ simple_rag/database/load_pdf.py
@@ -4,28 +4,28 @@ from pathlib import Path
 desktop_app = Path.home()/ "Desktop"
 apps = [app for app in desktop_app.iterdir() if app.is_file()]
 
+# def extract_load_pdf(file_name):
+
+#         # desktop_app = Path.home()/ "Desktop"
+#         # apps = [app for app in desktop_app.iterdir() if app.is_file()]
+
+#         for p in apps:
+#             if p.name.lower() == file_name.lower():
+#                 doc = fitz.open(p)
+#                 text= ""
+#                 for page in doc:
+#                    text += page.get_text()
+
+#                 return text
+#         raise FileNotFoundError(f"No such file: {file_name} in desktop")
+
 def extract_load_pdf(file_name):
+    """To read the pdf and store in a file"""
+    # path = search_file(file_name)
+    doc = fitz.open(file_name)
+    text= ""
+    for page in doc:
+        text += page.get_text()
 
-        # desktop_app = Path.home()/ "Desktop"
-        # apps = [app for app in desktop_app.iterdir() if app.is_file()]
-
-        for p in apps:
-            if p.name.lower() == file_name.lower():
-                doc = fitz.open(p)
-                text= ""
-                for page in doc:
-                   text += page.get_text()
-
-                return text
-        raise FileNotFoundError(f"No such file: {file_name} in desktop")
-
-# def load_pdf(file_name):
-#     """To read the pdf and store in a file"""
-#     # path = search_file(file_name)
-#     doc = fitz.open(file_name)
-#     text= ""
-#     for page in doc:
-#         text += page.get_text()
-
-#     return text
+    return text
     
