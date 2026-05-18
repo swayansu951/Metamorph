90dfecc90d8294ffbf395e7053ae3114b093b5a7
swayansu951 <swayansusahu951@gmail.com>
2026-05-18 21:03:50 +0530

    metamorpg -v 1.04.12

---
+++ run_server.py
@@ -0,0 +1,11 @@
+import asyncio
+import sys
+
+import uvicorn
+
+
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+
+uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
